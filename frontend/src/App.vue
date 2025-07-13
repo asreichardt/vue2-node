52fb4fe732a16b3f3e9a31aeaa4378a3f4425f3c
asreichardt <65123372+asreichardt@users.noreply.github.com>
2025-07-13 14:15:42 -0300

    inicial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Vue 2 + Node</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: 'Carregando...' };
+  },
+  mounted() {
+    fetch('/api/hello')
+      .then(r => r.json())
+      .then(d => this.message = d.message)
+      .catch(() => this.message = 'Erro na API');
+  }
+}
+</script>
